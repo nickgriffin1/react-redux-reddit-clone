78653d4f511816a90788ddd257f20acf44bdfb9a
Nick Griffin <ngriffin731@gmail.com>
2017-09-21 09:59:31 -0700

    Improved routing by including a 404 Not Found View for any route that doesn't match a defined route

--- client/src/components/MainView.js
+++ client/src/components/MainView.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import ListView from './ListView'
 import CategoryView from './CategoryView'
 import AddView from './AddView'
+import NotFoundView from './NotFoundView'
 
 class MainView extends Component {
   render() {
     return (
       <div>
-        <Route exact path='/' component={ListView} />
-        <Route exact path='/categories' component={CategoryView} />
-        <Route exact path='/add' component={AddView} />
-      </div>
+      	<Switch>
+	        <Route exact path='/' component={ListView} />
+	        <Route exact path='/categories' component={CategoryView} />
+	        <Route exact path='/add' component={AddView} />
+	        <Route component={NotFoundView} />
+	      </Switch>
+	    </div>
     )
   }
 }
