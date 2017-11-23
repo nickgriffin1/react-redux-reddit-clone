ea5dc105f34859cd310590a214dffa45ef41a773
Nick Griffin <ngriffin731@gmail.com>
2017-11-23 09:53:01 -0800

    Refactor CategoryView and NotFoundView into stateless functional components

--- client/src/components/CategoryView.js
+++ client/src/components/CategoryView.js
@@ -1,36 +1,34 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Grid, Row, Col, Glyphicon } from 'react-bootstrap'
 
-class CategoryView extends Component {
-  render() {
-    return (
-      <Grid>
-        <Row>
-          <Col className='category-row' xs={12}>
-            <h1 className='category-text'>View a Category</h1>
+const CategoryView = function(props) {
+  return (
+    <Grid>
+      <Row>
+        <Col className='category-row' xs={12}>
+          <h1 className='category-text'>View a Category</h1>
+        </Col>
+        {props.categories.map((category, index) => (
+          <Col key={index} className='category-row' xs={12}>
+            <Link to={category.path}>
+              <Row>
+                <h2 className='category-text'>
+                  <Col xs={11}>
+                    { category.name }
+                  </Col>
+                  <Col xs={1}>
+                    <Glyphicon glyph='chevron-right' />
+                  </Col>
+                </h2>
+              </Row>
+            </Link>
           </Col>
-          {this.props.categories.map((category, index) => (
-            <Col key={index} className='category-row' xs={12}>
-              <Link to={category.path}>
-                <Row>
-                  <h2 className='category-text'>
-                    <Col xs={11}>
-                      { category.name }
-                    </Col>
-                    <Col xs={1}>
-                      <Glyphicon glyph='chevron-right' />
-                    </Col>
-                  </h2>
-                </Row>
-              </Link>
-            </Col>
-          ))}
-        </Row>
-      </Grid>
-    )
-  }
+        ))}
+      </Row>
+    </Grid>
+  )
 }
 
 function mapStateToProps ({ categories }) {
--- client/src/components/NotFoundView.js
+++ client/src/components/NotFoundView.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Row, Col } from 'react-bootstrap'
 
-class NotFoundView extends Component {
-  render() {
-    return (
-      <Row>
-        <Col xs={12} className='text-center'>
-          <h1>404 Not Found</h1>
-          <h1>¯\_(ツ)_/¯</h1>
-        </Col>
-      </Row>
-    );
-  }
+const NotFoundView = function() {
+  return (
+    <Row>
+      <Col xs={12} className='text-center'>
+        <h1>404 Not Found</h1>
+        <h1>¯\_(ツ)_/¯</h1>
+      </Col>
+    </Row>
+  )
 }
 
 export default NotFoundView
